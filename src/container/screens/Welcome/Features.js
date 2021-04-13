c03e8a4016d961e85b8423f0d2a600d7f2fa2cb6
phamquyetthang <quyetthang.phucchi@gmail.com>
2021-04-13 18:55:28 +0700

    __async

--- src/container/screens/Welcome/Features.js
+++ src/container/screens/Welcome/Features.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-
+import translateIcon from "./language.svg"
 const Card = styled.div`
   ${tw`flex flex-col mx-auto max-w-xs items-center px-6 py-10 border-2 border-dashed border-primary-500 rounded-lg mt-12`}
   .imageContainer {
@@ -33,10 +33,10 @@ const FeatureComponent = () => {
         "Tra cứu các tài liệu Hán - Nôm trong kho tàng được chúng tôi lưu trữ",
     },
     {
-      icons: "fi-rr-cursor",
-      title: "Tham gia dán nhãn",
+      iconSvg: translateIcon,
+      title: "Từ điển",
       description:
-        "Tra cứu tài liệu Hán - Nôm dạng ảnh bằng cách sử dụng công cụ AI của chúng tôi",
+        "Tra cứu các tài liệu Hán - Nôm trong kho tàng được chúng tôi lưu trữ",
     },
     
     {
@@ -44,6 +44,12 @@ const FeatureComponent = () => {
       title: "Tra cứu nhanh",
       description:
         "Sử dụng công cự AI để quét nhanh tài liệu Hán - Nôm của bạn và chuyển thành văn bản chữ quốc ngữ",
+    },
+    {
+      icons: "fi-rr-cursor",
+      title: "Tham gia dán nhãn",
+      description:
+        "Tra cứu tài liệu Hán - Nôm dạng ảnh bằng cách sử dụng công cụ AI của chúng tôi",
     },
     {
       icons: "fi-rr-edit",
@@ -57,12 +63,6 @@ const FeatureComponent = () => {
       description:
         "Tra cứu tài liệu Hán - Nôm dạng ảnh bằng cách sử dụng công cụ AI của chúng tôi",
     },
-    {
-      icons: "fi-rr-mode-landscape",
-      title: "Tra cứu nhanh",
-      description:
-        "Tra cứu tài liệu Hán - Nôm dạng ảnh bằng cách sử dụng công cụ AI của chúng tôi",
-    },
   ];
 
   return (
@@ -73,7 +73,7 @@ const FeatureComponent = () => {
           {cards.map((card, i) => (
             <div key={i}>
               <Card className="cs-welcome-features-card">
-                <i className={`${card.icons} cs-welcome-icon`}></i>
+                {card.iconSvg ? <img src={card.iconSvg} alt="anh"/> : <i className={`${card.icons} cs-welcome-icon`}></i>}
                 <span className="textContainer">
                   <span className="title">{card.title || "Fully Secure"}</span>
                   <p className="description">
